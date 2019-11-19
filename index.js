8bae4e7bd219597fa4541e35a47307950bfec19f
gnardinosaur <github email address>
2019-11-19 10:13:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Nick is the champion";
