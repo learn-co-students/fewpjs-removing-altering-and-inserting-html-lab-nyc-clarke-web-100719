77452684304a25b56b7853bec1121dc4b1cc5893
jastaffen <jack.staffen@gmail.com>
2019-11-19 12:39:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.querySelector('main').remove();
+let newMarquee = document.createElement('marquee');
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Jack is the champion. Fuck you danny";
+document.body.appendChild(newMarquee);
+newMarquee.appendChild(newHeader);
