08750c9a611a8a8198bc9401e2225732f2da44f2
Ryan <rngao97@gmail.com>
2019-11-19 12:23:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove()
+document.querySelector("body")
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
+
