f885627ac2c5fbadefdaecf6d62edb6143c7e3ca
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-22 10:07:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector("#main").remove();
+let body = document.querySelector("body");
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Jeffrey Paulino is the champion";
+
+
